b8a2d2a181d16a6339817609ec3d84e9391019b7
aybatova <evaybatova@gmail.com>
2018-11-19 22:25:18 +0300

    The Comments were deleted, added "_" to sum-numbers, @org.junit.jupiter.api.Test was changed to @Test

--- src/main/java/ru/itpark/CreditService.java
+++ src/main/java/ru/itpark/CreditService.java
@@ -6,13 +6,13 @@ package ru.itpark;
         public double calculateCredit(double creditSum, double creditPeriod, double creditRate) {
 
 
-//            double creditPeriodMin = -1 * creditPeriod; первый вариант формулы
+
             double monthSum = creditRate/100/12;
 
-//            double pay = creditSum * (monthSum / (1 - Math.pow((1 + monthSum), creditPeriodMin))); первый вариант формулы
+
 
             double pay = creditSum * (monthSum + (monthSum /(Math.pow((1 + monthSum), creditPeriod) - 1)));
-//            return creditPeriod * Math.round(pay); выплаты за весь период не сходятся
+
             return Math.round(pay);
 
 
--- src/test/java/ru/itpark/CreditServiceTest.java
+++ src/test/java/ru/itpark/CreditServiceTest.java
@@ -1,17 +1,19 @@
 package ru.itpark;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreditServiceTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void calculateCredit() {
 
         {
             CreditService service = new CreditService();
 
             double paySum = service.calculateCredit(100_000,12, 10);
-            assertEquals(8792, paySum);
+            assertEquals(8_792, paySum);
         }
 
         {
@@ -24,33 +26,22 @@ class CreditServiceTest {
             CreditService service = new CreditService();
 
             double paySum = service.calculateCredit(100_000,20, 20);
-            assertEquals(5921, paySum);
+            assertEquals(5_921, paySum);
         }
 
         {
             CreditService service = new CreditService();
 
             double paySum = service.calculateCredit(100_000,6, 10);
-            assertEquals(17156, paySum);
+            assertEquals(17_156, paySum);
         }
 
         {
             CreditService service = new CreditService();
 
             double paySum = service.calculateCredit(100_000,3, 10);
-            assertEquals(33890, paySum);
+            assertEquals(33_890, paySum);
         }
-
-//        {
-//            CreditService service = new CreditService();
-//
-//            double paySum = service.calculateCredit(100_000,1, 10);
-//            assertEquals(100822, paySum);
-//        }
-
-
-
-
 
 
     }
