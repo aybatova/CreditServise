3f4b99143f6bf2fa2ac954f7fd0e19de910be2fa
aybatova <evaybatova@gmail.com>
2018-11-19 23:14:50 +0300

    change the type of variable for calculateCredit and Math.round(pay)

--- src/main/java/ru/itpark/CreditService.java
+++ src/main/java/ru/itpark/CreditService.java
@@ -3,12 +3,10 @@ package ru.itpark;
 
 
     public class CreditService {
-        public double calculateCredit(double creditSum, double creditPeriod, double creditRate) {
-
+        public long calculateCredit(double creditSum, double creditPeriod, double creditRate) {
 
 
             double monthSum = creditRate/100/12;
-
 
 
             double pay = creditSum * (monthSum + (monthSum /(Math.pow((1 + monthSum), creditPeriod) - 1)));
